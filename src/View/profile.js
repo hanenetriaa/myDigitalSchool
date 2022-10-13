4b802cb818707bfe12e516192e91df50406596eb
marieess <marie.espinosa@etu.univ-amu.fr>
2022-10-13 10:23:08 +0200

    fix bug profile

--- src/View/profile.js
+++ src/View/profile.js
@@ -3,59 +3,20 @@ import { useEffect, useState } from "react";
 import {getProfileById} from "../api/userService";
 
 const Profile = () => {;
- 
-  // const [nom, SetNom] = useState();
-  // const [prenom, SetPrenom] = useState();
-  // const [email, SetEmail] = useState();
-  // const [tel, SetTel] = useState();
-  // const [Id, SetId] = useState();
-  // const { id } = useParams();
-  // const getProfile = () => {
-  //   getProfileById(id)
-  //     .then((res) => {
-  //       console.log(res);
-  //       SetNom(res.data.nom);
-  //       SetPrenom(res.data.prenom);
-  //       SetEmail(res.data.email);
-  //       SetTel(res.data.tel);
-  //       SetId(res.data._id);
-  //       // SetId(res.data.data._id);
-  //     })
-  //     .catch((err) => {
-  //       console.log(err);
-  //     });
-  // };
-
-  // useEffect(() => {
-  //   const user = JSON.parse(localStorage.getItem("email"));
-  //   if (user) {
-  //     SetId(user.user._id);
-  //     console.log("id", user.user._id);
-  //   }
-  //   getProfile(id);
-  // }, []);
-
-  const [Prenom, SetPrenom] = useState();
-  const [Nom, SetNom] = useState();
-  const [Email, SetEmail] = useState();
-  const [Tel, SetTel] = useState();
-  const [id, SetId] = useState();
-
+  
+  const [Data, SetData] = useState({});
+  const { id } = useParams();
+  console.log(id);
   const getProfile = () => {
-    getProfileById()
+    getProfileById(id)
       .then((res) => {
         console.log(res);
-        SetPrenom(res.data.prenom);
-        SetNom(res.data.nom);
-        SetEmail(res.data.email);
-        SetTel(res.data.tel);
-        SetId(res.data._id);
+        SetData(res.data);
       })
       .catch((err) => {
         console.log(err);
       });
   };
-
   useEffect(() => {
     getProfile();
   }, []);
@@ -67,19 +28,19 @@ const Profile = () => {;
         <h1> Mon profile </h1>
         <section>
           Mon nom : 
-          <p>{Nom}</p>
+          <p>{Data.nom}</p>
         </section>
         <section>
           Mon prénom:
-        <p>{Prenom}</p>
+        <p>{Data.prenom}</p>
         </section>
         <section> 
           Mon mail : 
-          <p>{Email}</p>
+          <p>{Data.email}</p>
         </section>
         <section>
           Mon numéro de téléphone :
-          <p>{Tel}</p>
+          <p>{Data.mail}</p>
         </section>
       </section>
     </>
