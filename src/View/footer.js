b3b60e2343879129e2fad4e3ba876020218d6faa
marieess <marie.espinosa@etu.univ-amu.fr>
2022-10-13 11:31:21 +0200

    final commit v1

--- src/View/footer.js
+++ src/View/footer.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import './footer.css';
 
 
@@ -8,17 +9,16 @@ function Footer() {
         <div>
         <footer>
           <ul className="icons">
-<<<<<<< HEAD
+
             <li><a href="https://www.facebook.com/my.digital.schoolFR/" target="blank">Facebook</a></li>
             <li><a href="https://www.instagram.com/mydigitalschool/" target="blank"> Instagram</a></li>
             <li><a href="https://www.linkedin.com/school/mydigitalschool/" target="blank">Linkedin</a></li>
             <li><a href="https://twitter.com/home" target="blank">Twitter</a></li>
-=======
           </ul>
           <ul className="menu">
-            <li><a href="#">Home</a></li>
-            <li><a href="#">Mon profile</a></li>
-            <li><a href="#">Log out</a></li>
+          
+            <li><Link to={"/home"} >Home </Link></li>
+            <li><Link to={"/profile"} >Mon profile </Link></li>
           </ul>
           <div className="footer-copyright">
             <p>Un site réalisé par les meilleurs dev web de la planète</p>
